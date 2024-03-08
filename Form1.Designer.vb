d8944eedd12ec8afc19d7b29c038353d6a80bf57
Jeevy Seguerra <jeevyseguerra6@gmail.com>
2024-03-08 18:10:18 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonLogin = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.labelTitle = New System.Windows.Forms.Label()
+        Me.TextBoxEmail = New System.Windows.Forms.TextBox()
+        Me.TextBoxPassword = New System.Windows.Forms.TextBox()
+        Me.Register = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ButtonLogin
+        '
+        Me.ButtonLogin.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ButtonLogin.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ButtonLogin.FlatAppearance.BorderSize = 0
+        Me.ButtonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonLogin.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonLogin.Location = New System.Drawing.Point(494, 344)
+        Me.ButtonLogin.Margin = New System.Windows.Forms.Padding(0)
+        Me.ButtonLogin.Name = "ButtonLogin"
+        Me.ButtonLogin.Size = New System.Drawing.Size(313, 36)
+        Me.ButtonLogin.TabIndex = 0
+        Me.ButtonLogin.Text = "Login"
+        Me.ButtonLogin.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.labelTitle)
+        Me.Panel1.Location = New System.Drawing.Point(-1, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(391, 588)
+        Me.Panel1.TabIndex = 1
+        '
+        'labelTitle
+        '
+        Me.labelTitle.AutoSize = True
+        Me.labelTitle.Font = New System.Drawing.Font("Tahoma", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelTitle.Location = New System.Drawing.Point(131, 259)
+        Me.labelTitle.Name = "labelTitle"
+        Me.labelTitle.Size = New System.Drawing.Size(133, 42)
+        Me.labelTitle.TabIndex = 0
+        Me.labelTitle.Text = "BASTA"
+        '
+        'TextBoxEmail
+        '
+        Me.TextBoxEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxEmail.ForeColor = System.Drawing.Color.Silver
+        Me.TextBoxEmail.Location = New System.Drawing.Point(494, 159)
+        Me.TextBoxEmail.Name = "TextBoxEmail"
+        Me.TextBoxEmail.Size = New System.Drawing.Size(313, 29)
+        Me.TextBoxEmail.TabIndex = 2
+        Me.TextBoxEmail.Text = "Enter Email..."
+        '
+        'TextBoxPassword
+        '
+        Me.TextBoxPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxPassword.ForeColor = System.Drawing.Color.Silver
+        Me.TextBoxPassword.Location = New System.Drawing.Point(494, 243)
+        Me.TextBoxPassword.Name = "TextBoxPassword"
+        Me.TextBoxPassword.Size = New System.Drawing.Size(313, 29)
+        Me.TextBoxPassword.TabIndex = 3
+        Me.TextBoxPassword.Text = "Enter Password..."
+        '
+        'Register
+        '
+        Me.Register.BackColor = System.Drawing.Color.Goldenrod
+        Me.Register.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Register.FlatAppearance.BorderSize = 0
+        Me.Register.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Register.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Register.Location = New System.Drawing.Point(715, 490)
+        Me.Register.Name = "Register"
+        Me.Register.Size = New System.Drawing.Size(92, 30)
+        Me.Register.TabIndex = 4
+        Me.Register.Text = "Register"
+        Me.Register.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(565, 501)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(144, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Don't have an account?"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Maroon
+        Me.ClientSize = New System.Drawing.Size(908, 586)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Register)
+        Me.Controls.Add(Me.TextBoxPassword)
+        Me.Controls.Add(Me.TextBoxEmail)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.ButtonLogin)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonLogin As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents labelTitle As Label
+    Friend WithEvents TextBoxEmail As TextBox
+    Friend WithEvents TextBoxPassword As TextBox
+    Friend WithEvents Register As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports System.Drawing.Drawing2D
+
+Public Class Form1
+    Private dragging As Boolean
+    Private offsetX, offsetY As Integer
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        If e.Button = MouseButtons.Left Then
+            dragging = True
+            offsetX = e.X
+            offsetY = e.Y
+        End If
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If dragging Then
+            Dim newLocation As New Point(Me.Left + e.X - offsetX, Me.Top + e.Y - offsetY)
+            Me.Location = newLocation
+        End If
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        If e.Button = MouseButtons.Left Then
+            dragging = False
+        End If
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonLogin.Click
+
+    End Sub
+
+
+    Private Sub ButtonLog_Paint(sender As Object, e As PaintEventArgs) Handles ButtonLogin.Paint
+        Dim path As New GraphicsPath()
+        Dim rect As Rectangle = ButtonLogin.ClientRectangle
+        Dim radius As Integer = 40
+        path.AddArc(rect.X, rect.Y, radius, radius, 180, 90)
+        path.AddArc(rect.Right - radius, rect.Y, radius, radius, 270, 90)
+        path.AddArc(rect.Right - radius, rect.Bottom - radius, radius, radius, 0, 90)
+        path.AddArc(rect.X, rect.Bottom - radius, radius, radius, 90, 90)
+        path.CloseFigure()
+
+        ButtonLogin.Region = New Region(path)
+    End Sub
+    Private Sub ButtonReg_Paint(sender As Object, e As PaintEventArgs) Handles Register.Paint
+        Dim path As New GraphicsPath()
+        Dim rect As Rectangle = Register.ClientRectangle
+        Dim radius As Integer = 20
+        path.AddArc(rect.X, rect.Y, radius, radius, 180, 90)
+        path.AddArc(rect.Right - radius, rect.Y, radius, radius, 270, 90)
+        path.AddArc(rect.Right - radius, rect.Bottom - radius, radius, radius, 0, 90)
+        path.AddArc(rect.X, rect.Bottom - radius, radius, radius, 90, 90)
+        path.CloseFigure()
+
+        Register.Region = New Region(path)
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim path As New GraphicsPath()
+        path.AddArc(0, 0, 20, 20, 180, 90)
+        path.AddArc(Me.Width - 20, 0, 20, 20, 270, 90)
+        path.AddArc(Me.Width - 20, Me.Height - 20, 20, 20, 0, 90)
+        path.AddArc(0, Me.Height - 20, 20, 20, 90, 90)
+        Me.Region = New Region(path)
+    End Sub
+
+    Private Sub TextBoxEmail_Enter(sender As Object, e As EventArgs) Handles TextBoxEmail.Enter
+        If TextBoxEmail.Text = "Enter Email..." Then
+            TextBoxEmail.Text = ""
+            TextBoxEmail.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub TextBoxEmail_Leave(sender As Object, e As EventArgs) Handles TextBoxEmail.Leave
+        If TextBoxEmail.Text = "" Then
+            TextBoxEmail.Text = "Enter Email..."
+            TextBoxEmail.ForeColor = Color.Silver
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Register.Click
+        Dim registerForm As New Register()
+        registerForm.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub TextBoxPassword_Enter(sender As Object, e As EventArgs) Handles TextBoxPassword.Enter
+        If TextBoxPassword.Text = "Enter Password..." Then
+            TextBoxPassword.Text = ""
+            TextBoxPassword.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub TextBoxPassword_Leave(sender As Object, e As EventArgs) Handles TextBoxPassword.Leave
+        If TextBoxPassword.Text = "" Then
+            TextBoxPassword.Text = "Enter Password..."
+            TextBoxPassword.ForeColor = Color.Silver
+        End If
+    End Sub
+
+    Private Sub TextBoxEmail_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxEmail.KeyPress
+        If Asc(e.KeyChar) = 13 Then
+            TextBoxPassword.Focus()
+        End If
+    End Sub
+End Class
